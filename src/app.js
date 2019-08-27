a43b4849a58ed4ddd542ed77072b5fa5f6c4e7da
Alexey Stepankov <xeurun@yahoo.com>
2019-08-28 00:01:42 +0400

    Add settings tab, update infra.
    * feat (settings) added tabs
    * chore (docker) locked node version

--- src/app.js
+++ src/app.js
@@ -13,83 +13,89 @@ require('angular-hotkeys');
 
 import './app.scss';
 
-const APP_VERSION = '1.0.8';
+const APP_VERSION = '1.1.0';
 
 angular
-    .module(
-        'quiz-client',
-        [
-          'ngMaterial',
-          'ngSanitize',
-          'ngCookies',
-          'angular-intro',
-          'hljs',
-          'LocalStorageModule',
-          'cfp.hotkeys',
-        ]
-    )
-    .config(
-        function(
-            $mdIconProvider,
-            $interpolateProvider,
-            $locationProvider,
-            $sceDelegateProvider,
-            $httpProvider,
-            localStorageServiceProvider,
-            hotkeysProvider
-        ) {
-          $interpolateProvider.startSymbol('[[ ').endSymbol(' ]]');
-          $locationProvider.html5Mode(true).hashPrefix('*');
-          $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
-          $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-          $mdIconProvider.fontSet('md', 'material-icons');
-          $sceDelegateProvider.resourceUrlWhitelist([
-            'self',
-            'https://docs.google.com/**',
-          ]);
-          hotkeysProvider.useNgRoute = false;
-          localStorageServiceProvider
-              .setPrefix('quiz-client')
-              .setStorageType('localStorage')
-              .setNotify(false, false);
-        }
-    )
-    .value('APP_VERSION', APP_VERSION)
-    .run(function(QuizFactory) {
-      QuizFactory.init();
-    })
-    .filter('orderObjectBy', function() {
-      return function(items, field, reverse) {
-        const filtered = [];
-        angular.forEach(items, function(item) {
-          filtered.push(item);
-        });
-        filtered.sort(function(a, b) {
-          return (a[field] > b[field] ? 1 : -1);
-        });
-        if (reverse) filtered.reverse();
+  .module(
+    'quiz-client',
+    [
+      'ngMaterial',
+      'ngSanitize',
+      'ngCookies',
+      'angular-intro',
+      'hljs',
+      'LocalStorageModule',
+      'cfp.hotkeys',
+    ]
+  )
+  .config(
+    function (
+      $mdIconProvider,
+      $interpolateProvider,
+      $locationProvider,
+      $sceDelegateProvider,
+      $httpProvider,
+      $mdThemingProvider,
+      localStorageServiceProvider,
+      hotkeysProvider
+    ) {
+      $interpolateProvider.startSymbol('[[ ').endSymbol(' ]]');
+      $locationProvider.html5Mode(true).hashPrefix('*');
+      $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+      $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+      $mdIconProvider.fontSet('md', 'material-icons');
+      $sceDelegateProvider.resourceUrlWhitelist([
+        'self',
+        'https://docs.google.com/**',
+      ]);
+      hotkeysProvider.useNgRoute = false;
+      localStorageServiceProvider
+        .setPrefix('quiz-client')
+        .setStorageType('localStorage')
+        .setNotify(false, false);
 
-        return filtered;
-      };
-    })
-    .filter('prettyDate', function() {
-      return function(val) {
-        const date = new Date(val);
+      $mdThemingProvider.enableBrowserColor({
+        theme: 'default',
+        palette: 'primary',
+        hue: '200'
+      });
+    }
+  ).run(function (QuizFactory) {
+    QuizFactory.init();
+  })
+  .value('APP_VERSION', APP_VERSION)
+  .filter('orderObjectBy', function () {
+    return function (items, field, reverse) {
+      const filtered = [];
+      angular.forEach(items, function (item) {
+        filtered.push(item);
+      });
+      filtered.sort(function (a, b) {
+        return (a[field] > b[field] ? 1 : -1);
+      });
+      if (reverse) filtered.reverse();
 
-        return date.toISOString().slice(0, 10) + ' ' + date.toISOString().slice(11, 19);
-      };
-    })
-    .filter('prettyForm', function() {
-      return function(val) {
-        if (!val) {
-          return val;
-        }
+      return filtered;
+    };
+  })
+  .filter('prettyDate', function () {
+    return function (val) {
+      const date = new Date(val);
 
-        return val.replace(new RegExp('\n', 'g'), '<br>');
-      };
-    })
-    .controller('AnswerToastController', require('./Controllers/AnswerToastController.js'))
-    .controller('DialogController', require('./Controllers/DialogController.js'))
-    .controller('IntroController', require('./Controllers/IntroController.js'))
-    .controller('QuizController', require('./Controllers/QuizController.js'))
-    .factory('QuizFactory', require('./Factory/QuizFactory.js'));
+      return date.toISOString().slice(0, 10) + ' ' + date.toISOString().slice(11, 19);
+    };
+  })
+  .filter('prettyForm', function () {
+    return function (val) {
+      if (!val) {
+        return val;
+      }
+
+      return val.replace(new RegExp('\n', 'g'), '<br>');
+    };
+  })
+  .controller('AnswerToastController', require('./Controllers/AnswerToastController.js'))
+  .controller('DialogController', require('./Controllers/DialogController.js'))
+  .controller('IntroController', require('./Controllers/IntroController.js'))
+  .controller('QuizController', require('./Controllers/QuizController.js'))
+  .factory('QuizFactory', require('./Factory/QuizFactory.js'));
